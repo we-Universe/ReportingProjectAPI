b38d0828a12a75bbf77b9dfce89473b7aa0a935f
Mayar Abuzahra <79359366+Mayar-Abuzahra@users.noreply.github.com>
2023-12-20 16:44:25 +0200

    add reports model

---
+++ ReportingProject/Data/Models/ReportsModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReportingProject.Data.Models
+{
+	public class ReportsModel
+	{
+        [Required]
+        public int TypeID { get; set; }
+
+        public DateTime? LastModified { get; set; }
+
+        [Required]
+        public int ApprovalStatusID { get; set; }
+
+        public string Notes { get; set; } = string.Empty;
+
+        public int Month { get; set; }
+
+        public int Year { get; set; }
+
+        [Required]
+        public byte[]? ReportFile { get; set; }
+    }
+}
